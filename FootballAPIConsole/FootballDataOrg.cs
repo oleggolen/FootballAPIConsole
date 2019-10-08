6457e746516149e1172e851010c1b946a2c10801
judgen2005 <49545597+judgen2005@users.noreply.github.com>
2019-10-08 11:03:23 +0700

    Добавлен класс FootballDataOrg. Изменен код Main

---
+++ FootballAPIConsole/FootballDataOrg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballAPIConsole
+{
+    class FootballDataOrg
+    {
+        public class Welcome
+        {
+            public List<Competition> Competitions { get; set; }
+        }
+
+        public class Competition
+        {
+            public Area Area { get; set; }
+            public string Name { get; set; }
+            public CurrentSeason CurrentSeason { get; set; }
+            public DateTimeOffset LastUpdated { get; set; }
+        }
+
+        public class Area
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CurrentSeason
+        {
+            public DateTimeOffset StartDate { get; set; }
+            public DateTimeOffset EndDate { get; set; }
+        }
+    }
+}
--- FootballAPIConsole/Program.cs
+++ FootballAPIConsole/Program.cs
@@ -13,18 +13,30 @@ namespace FootballAPIConsole
     {
         static void Main(string[] args)
         {
-            var key = ConfigurationSettings.AppSettings["apikey"];
-            HttpClient client = new HttpClient();
-            var json =client.GetStringAsync("https://apifootball.com/api/?action=get_countries&APIkey=" + key).Result;
-            var countries = JsonConvert.DeserializeObject<IEnumerable<County>>(json);
-            json = client.GetStringAsync("https://apifootball.com/api/?action=get_leagues&country_id=169&APIkey=" +
-                key).Result;
-            var leagues = JsonConvert.DeserializeObject<IEnumerable<League>>(json);
-            json = client.GetStringAsync(
-                "https://apifootball.com/api/?action=get_events&from=2018-05-13&to=2018-05-13&league_id=169&APIkey=" +
-                key).Result;
-                var match = JsonConvert.DeserializeObject<IEnumerable<Match>>(json);
+            //var key = ConfigurationSettings.AppSettings["apikey"];
+            //HttpClient client = new HttpClient();
+            //var json =client.GetStringAsync("https://apifootball.com/api/?action=get_countries&APIkey=" + key).Result;
+            //var countries = JsonConvert.DeserializeObject<IEnumerable<County>>(json);
+            //json = client.GetStringAsync("https://apifootball.com/api/?action=get_leagues&country_id=169&APIkey=" +
+            //    key).Result;
+            //var leagues = JsonConvert.DeserializeObject<IEnumerable<League>>(json);
+            //json = client.GetStringAsync(
+            //    "https://apifootball.com/api/?action=get_events&from=2018-05-13&to=2018-05-13&league_id=169&APIkey=" +
+            //    key).Result;
+            //    var match = JsonConvert.DeserializeObject<IEnumerable<Match>>(json);
 
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-Auth-Token", "5312257dd48a4aadb0c0dac2cb9e6432");
+            var resfootball = client.GetStringAsync($"http://api.football-data.org/v2/competitions/").Result;
+            var jsonfootball = JsonConvert.DeserializeObject<FootballDataOrg.Welcome>(resfootball);
+            Console.WriteLine($"Name      : {jsonfootball.Competitions[0].Name}");
+            Console.WriteLine($"Area      : {jsonfootball.Competitions[0].Area.Name}");
+            Console.WriteLine($"Id        : {jsonfootball.Competitions[0].Area.Id}");
+            Console.WriteLine($"StartDate : {jsonfootball.Competitions[0].CurrentSeason.StartDate}");
+            Console.WriteLine($"EndDate   : {jsonfootball.Competitions[0].CurrentSeason.EndDate}");
+            Console.WriteLine($"Updated   : {jsonfootball.Competitions[0].LastUpdated}");
+
+            Console.ReadKey();
         }
     }
 }
